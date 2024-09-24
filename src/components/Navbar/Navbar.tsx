ccd2c529c0f17144683ee8f3a78af8f54ba51394
DamiCode <cdamiang90@gmail.com>
2024-09-24 00:07:25 -0300

    cambios en el navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -106,21 +106,21 @@ const Navbar = () => {
           <div className="text-center px-2 pt-2 pb-3 space-y-1 sm:px-3">
             <Link
               href="/"
-              className="mt-1 hover:bg-secondary hover:text-white rounded-lg text-base font-medium bg-primary p-2"
+              className="mt-1 hover:bg-secondary hover:text-white block rounded-lg text-base font-medium bg-primary p-2"
               onClick={closeNavbar}
             >
               INICIO
             </Link>
             <Link
               href="/products"
-              className="hover:bg-secondary hover:text-white  rounded-lg text-base font-medium bg-primary p-2"
+              className="hover:bg-secondary hover:text-white block rounded-lg text-base font-medium bg-primary p-2"
               onClick={closeNavbar}
             >
               PRODUCTOS
             </Link>
             <Link
               href="/about"
-              className="hover:bg-secondary hover:text-white rounded-lg p-2"
+              className="hover:bg-secondary hover:text-white block rounded-lg text-base font-medium bg-primary p-2"
             >
               SOBRE NOSOTROS
             </Link>
